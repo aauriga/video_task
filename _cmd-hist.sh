5a219ee4425ddbd2fb3b989cfdbe3a0862e37543
Marian Dumitrascu <md@dumitrascu.net>
2023-02-20 14:14:40 -0600

    first commit

---
+++ _cmd-hist.sh
@@ -0,0 +1,10 @@
+
+conda create -n django-tasks
+
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/PredictIf/mogas-video-tasks.git
+git push -u origin main
