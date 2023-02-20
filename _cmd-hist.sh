3019c91ac031ab356c2e283b380be5870f0233a3
Marian Dumitrascu <md@dumitrascu.net>
2023-02-20 14:17:34 -0600

    skdjfk

--- _cmd-hist.sh
+++ _cmd-hist.sh
@@ -8,3 +8,9 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/PredictIf/mogas-video-tasks.git
 git push -u origin main
+
+# TODO: #1 install django
+# TODO: #2 setup admin
+
+git fetch origin
+git checkout 1-install-django
