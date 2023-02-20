6434cee9c210ee3064df0e8e18b9ef149bb38f82
AZ Instance <md@dumitrascu.net>
2023-02-20 20:46:07 +0000

    installed

--- _cmd-hist.sh
+++ _cmd-hist.sh
@@ -14,3 +14,40 @@ git push -u origin main
 
 git fetch origin
 git checkout 1-install-django
+
+
+docker run -itd \
+    -p 9999:8888 \
+    --name devbox-x64 \
+    --restart always \
+    --privileged \
+    --platform linux/amd64 \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v ~/.aws:/root/.aws \
+    -v ~/Dropbox/work-0/-general/qc:/root/notebooks/QC \
+    -v /Users/marian.dumitrascu/Dropbox/work-P/video-tasks:/root/video-tasks \
+    predictif/devbox-x64:001 /bin/bash -c "\
+    jupyter notebook \
+    --notebook-dir=~/notebooks --ip='*' --port=8888 \
+    --no-browser --allow-root"
+
+# ################################################################################
+# install django
+
+pip install django
+
+django-admin startproject mainsite .
+
+python manage.py startapp video_tasks
+
+python manage.py runserver 8080
+
+python manage.py runserver
+
+python manage.py makemigrations
+
+python manage.py migrate
+
+python manage.py createsuperuser
+
+# password is: ....123
